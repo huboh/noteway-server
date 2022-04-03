2a0a78059e284014d3833d1cf1d1941e41fe916e
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-03 10:14:38 +0100

    FEAT - 😎 create express app & apollo server

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import Database from './Database';
+import { startServer } from './utils';
+
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
+
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+
+const { PORT, MONGO_CONNECTION_STRING } = process.env;
+
+const app = express();
+const host = 'localhost';
+const port = PORT || 5000;
+const database = new Database('hey');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({
+    userId: req.headers.authorization
+  })
+});
+
+
+database.connect(MONGO_CONNECTION_STRING!, {
+  onDisconnect: () => {
+    console.log('database connection lost');
+    process.exit(0);
+  },
+
+  onError: (error) => {
+    console.log(`database${error ? ' initial ' : ' '}connection error`);
+    process.exit(1);
+  },
+
+  onOpen: async () => {
+    console.log('connection to database established 🥳');
+
+    await server.start();
+    server.applyMiddleware({ app, path: '/api/graphql' });
+
+    startServer({ app, host, port, server });
+  }
+});
