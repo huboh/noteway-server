5c0b16353eadb9d2d31762efa96ea79325b6b00c
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-03 10:06:02 +0100

    REFACTOR - 🙈 add a default object export to `uuid` utils module

--- src/utils/uuid/index.ts
+++ src/utils/uuid/index.ts
@@ -3,6 +3,11 @@ import { v4, validate } from 'uuid';
 export const createUuid = () => v4();
 export const validateUuid = (uuid: string) => validate(uuid);
 
+export default {
+  createUuid,
+  validateUuid
+};;
+
 // * i want the module to have a consistent api, that's why im avoiding the style below 🙄
 // export { v4 as createUuid } from 'uuid';
 // export { validate as validateUuid } from 'uuid';
