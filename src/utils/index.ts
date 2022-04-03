8ba6ba77cabd28d9c4a500bb947bf73ef56ec08a
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-03 09:13:54 +0100

    😁 FEAT - add `startServer` util funtion for starting the server

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,8 @@
 import { StartServerProps } from "../types";
 
 export * from './uuid';
+
+export const startServer = ({ app, port, host, server }: StartServerProps) => {
+  app.listen(Number(port), host, () => console.log(`app listening on ${host}:${port}${server?.graphqlPath}`)
+  );
+};
