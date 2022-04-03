56be8ca1d9fef8cb7858851edae161fd3683a918
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-03 09:10:48 +0100

    🤓 FEAT - create `constants` module for project wide constants

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,4 @@
+import { NoteVisibility, ThemePreference } from "../../types";
+
+export const DEFAULT_NOTE_VISIBILITY = NoteVisibility.PUBLIC;
+export const DEFAULT_THEME_PREFERENCE = ThemePreference.SYSTEM;
