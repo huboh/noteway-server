67ad9cdc889ca5b64d10c4d10624a3600f6af6b0
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-03 10:07:13 +0100

    FIX - ☕ remove `uuid` re-export from utils module

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 import { StartServerProps } from "../types";
 
-export * from './uuid';
-
 export const startServer = ({ app, port, host, server }: StartServerProps) => {
   app.listen(Number(port), host, () => console.log(`app listening on ${host}:${port}${server?.graphqlPath}`)
   );
