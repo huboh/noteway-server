70a2a8d6b2df6e06d6af24f2327ea77affa07251
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-04 13:38:33 +0100

    FEAT - add new constants to `constants` module

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,4 +1,10 @@
 import { NoteVisibility, ThemePreference } from "../../types";
 
+const { NODE_ENV } = process.env;
+
+export const PAGINATION_LIMIT = 100;
+export const PAGINATION_INITIAL_PAGE = 1;
+export const isDevelopment = NODE_ENV === "development";
 export const DEFAULT_NOTE_VISIBILITY = NoteVisibility.PUBLIC;
 export const DEFAULT_THEME_PREFERENCE = ThemePreference.SYSTEM;
+export const DEFAULT_SERVER_ERROR_MESSAGE = "Oops! Something went wrong! :(";
