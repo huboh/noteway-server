2f6a5f949818daff517449ca9988d9e9fc922cc9
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-05 15:04:29 +0100

    👷‍♂️ REFACTOR - remove redundant code from `tokens` module

--- src/utils/tokens/index.ts
+++ src/utils/tokens/index.ts
@@ -2,20 +2,20 @@ import jsonWebToken, { SignOptions, VerifyOptions } from 'jsonwebtoken';
 
 const { TOKEN_SECRET } = process.env;
 
-const generateToken = async (payload: object | any[], options: SignOptions = {}) => {
-  return jsonWebToken.sign(payload, TOKEN_SECRET!, {
+const generateToken = (payload: object | any[], options: SignOptions = {}) => (
+  jsonWebToken.sign(payload, TOKEN_SECRET!, {
     expiresIn: 28800, // 8hrs in seconds
     ...options
-  });
-};
+  })
+);
 
-const verifyToken = async (token: string, options: VerifyOptions = {}) => {
-  return jsonWebToken.verify(token, TOKEN_SECRET!, {
+const verifyToken = (token: string, options: VerifyOptions = {}) => (
+  jsonWebToken.verify(token, TOKEN_SECRET!, {
     ...options
-  });
-};
+  })
+);
 
 export default {
   verifyToken,
   generateToken,
-};
+};;
