cb0fca27c3ef6c4d079e6462bf057c983175d054
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-05 16:41:56 +0100

    😬 FEAT - create util function `validateMongoObjectId` for validating mongoDB identifiers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,20 @@
-import { StartServerProps } from "../types";
+import errors from "./errors";
+import { StartServerProps, } from "../types";
 import { isDevelopment, DEFAULT_SERVER_ERROR_MESSAGE } from '../utils/constants';
 
+import { isValidObjectId } from 'mongoose';
 import { ApolloServerExpressConfig } from "apollo-server-express";
 
 export const startServer = ({ app, port, host, server }: StartServerProps) => {
   app.listen(Number(port), host, () => console.log(`app listening on ${host}:${port}${server?.graphqlPath}`)
   );
 };
+
+export const validateMongoObjectId = (...ids: unknown[]) => ids.forEach(id => {
+  if (id && !isValidObjectId(id)) {
+    throw new errors.ValidationError('invalid idenitifier');
+  }
+});
 
 export const formatError: ApolloServerExpressConfig['formatError'] = (error) => ({
   message: error.message || DEFAULT_SERVER_ERROR_MESSAGE,
