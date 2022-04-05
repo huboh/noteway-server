fc6586793b318655681cd5a90f4ea5a1065eb36e
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-05 16:38:37 +0100

    😬 FEAT - create util function `formatError` for formatting thrown errors for graphql

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,17 @@
 import { StartServerProps } from "../types";
+import { isDevelopment, DEFAULT_SERVER_ERROR_MESSAGE } from '../utils/constants';
+
+import { ApolloServerExpressConfig } from "apollo-server-express";
 
 export const startServer = ({ app, port, host, server }: StartServerProps) => {
   app.listen(Number(port), host, () => console.log(`app listening on ${host}:${port}${server?.graphqlPath}`)
   );
 };
+
+export const formatError: ApolloServerExpressConfig['formatError'] = (error) => ({
+  message: error.message || DEFAULT_SERVER_ERROR_MESSAGE,
+  path: error.path,
+  locations: error.locations,
+  stack: isDevelopment ? (error.stack?.split?.('\n') ?? []) : undefined,
+  extensions: isDevelopment ? error.extensions : undefined,
+});
