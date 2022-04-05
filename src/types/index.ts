689c33753073a8e6756664475b2f1a54b9b78856
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-05 15:59:30 +0100

    add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,13 @@
+import { context } from "..";
 import { Express } from "express";
+import { Model, FilterQuery } from 'mongoose';
 import { ApolloServer } from "apollo-server-express";
+
+export type AuthType = 'Bearer' | 'Basic';
+
+export type Context = Awaited<ReturnType<typeof context>>;
+
+export type Resolvers<S, A, C, I> = { [T: string]: (source: S, argument: A, context: C, info: I) => unknown; };
 
 export const enum NoteVisibility {
   PUBLIC = 'PUBLIC',
@@ -10,6 +18,17 @@ export const enum ThemePreference {
   DARK = 'dark',
   LIGHT = 'light',
   SYSTEM = 'system'
+}
+
+export interface Token {
+  userId: string;
+}
+
+export interface PaginateQueryProps<T> {
+  page: number;
+  limit: number;
+  model: Model<T>;
+  query: FilterQuery<T>;
 }
 
 export interface StartServerProps {
@@ -36,4 +55,47 @@ export interface ConnectProps {
    * handler for `disconnected` event, emitted when mongoose disconnects from MongoDB
    */
   onDisconnect?(): void;
+}
+
+export interface UserExistsQueryFilter {
+  id?: string;
+  email?: string;
+  username?: string;
+}
+
+export interface GetUserQueryFilter {
+  id?: string;
+  email?: string;
+  username?: string;
+}
+
+export interface DeleteUserQueryFilter {
+  id?: string;
+  email?: string;
+  username?: string;
+}
+
+export interface SignupCredentials {
+  email?: string;
+  username?: string;
+  password: string;
+}
+
+export interface LoginCredentials {
+  email?: string;
+  username?: string;
+  password: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+  avatarUrl: string;
+  isEmailVerified: boolean;
+  preferences: {
+    theme: string;
+  };
 }
