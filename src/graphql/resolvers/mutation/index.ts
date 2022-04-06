8b94cd656f8668ed7d0e0d3629e9534f2495cdf4
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-06 13:19:19 +0100

    FEAT - add mutation resolvers for `User` type

--- src/graphql/resolvers/mutation/index.ts
+++ src/graphql/resolvers/mutation/index.ts
@@ -1,4 +1,33 @@
-const mutation = {};
+import { SignupCredentials, LoginCredentials, Context } from '../../../types';
+
+const mutation = {
+  userDelete(source: undefined, variables: any, context: Context) {
+    const { userId } = variables;
+    const { database } = context;
+
+    return database.User.deleteUser({
+      id: userId
+    });
+  },
+
+  userSignup(source: undefined, variables: any, context: Context) {
+    const { password, email, username } = variables.signupCredentials as SignupCredentials;
+    const { database } = context;
+
+    return database.User.signupUser({
+      password, email, username
+    });
+  },
+
+  userLogin(source: undefined, variables: any, context: Context) {
+    const { password, email, username } = variables.loginCredentials as LoginCredentials;
+    const { database } = context;
+
+    return database.User.loginUser({
+      password, email, username
+    });
+  }
+};
 
 export {
   mutation as default
