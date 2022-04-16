2f777224e8f0dd02b754fe8cacd945405a639de6
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-16 23:37:39 +0100

    🤨 REFACTOR : add global constants to `constants` module

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,11 +1,32 @@
-import { NoteVisibility, ThemePreference } from "../../types";
+import {
+  NoteVisibility,
+  ThemePreference,
+  SessionErrorMessage,
+  NoteSorting,
+  CollaboratorRole
+} from "../../types";
 
 const { NODE_ENV } = process.env;
 
+export const isDevelopment = NODE_ENV === "development";
+export const DEFAULT_SERVER_ERROR_MESSAGE = "Oops! Something went wrong! :(";
 
+// Sessions
+export const SESSION_EXPIRED_MESSAGE: SessionErrorMessage = 'Session Expired';
+export const SESSION_INVALID_MESSAGE: SessionErrorMessage = 'Invalid Session';
+
+// Note
+export const DEFAULT_NOTE_SORTING: NoteSorting = 'updatedAt';
+export const DEFAULT_THEME_PREFERENCE: ThemePreference = 'system';
+export const DEFAULT_NOTE_VISIBILITY: NoteVisibility = 'private';
+
+// id
+export const INVALID_ID_ERROR_MESSAGE = 'invalid identifier';
+
+// Collaborator
+export const DEFAULT_COLLABORATOR_LIMIT = 10;
+export const DEFAULT_COLLABORATOR_ROLE: CollaboratorRole = 'viewer';
+
+// query pagination
 export const PAGINATION_LIMIT = 100;
 export const PAGINATION_INITIAL_PAGE = 1;
-export const isDevelopment = NODE_ENV === "development";
-export const DEFAULT_NOTE_VISIBILITY: NoteVisibility = 'public';
-export const DEFAULT_THEME_PREFERENCE = ThemePreference.SYSTEM;
-export const DEFAULT_SERVER_ERROR_MESSAGE = "Oops! Something went wrong! :(";
