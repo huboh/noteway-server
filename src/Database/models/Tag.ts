7412847d36a89d0034770d0b53fc10b337814101
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-17 00:29:40 +0100

    🤗 FEAT : create `Tag` collection schema and model

---
+++ src/Database/models/Tag.ts
@@ -0,0 +1,28 @@
+import { Tag } from '../../types';
+
+import { Schema, model } from 'mongoose';
+
+const TagSchema = new Schema({
+  tagId: {
+    type: String, required: true, unique: true
+  },
+  userId: {
+    type: String, required: true
+  },
+  color: {
+    type: String, default: null, maxlength: 12
+  },
+  label: {
+    type: String, required: true, maxlength: 120, unique: true
+  },
+},
+  {
+    timestamps: true,
+  }
+);
+
+const Tag = model<Tag>('Tag', TagSchema);
+
+export {
+  Tag as default
+};
