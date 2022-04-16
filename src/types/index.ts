bfc750fc7550e406d073caea1860c5d1b476f116
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-16 23:25:05 +0100

    REFACTOR : add and update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,21 +1,23 @@
 import { context } from "..";
 import { Express } from "express";
-import { Model, FilterQuery } from 'mongoose';
 import { ApolloServer } from "apollo-server-express";
+import { Model, FilterQuery, Document } from 'mongoose';
 
 export type AuthType = 'Bearer' | 'Basic';
 
 export type NoteVisibility = 'public' | 'private';
 
+export type ThemePreference = 'dark' | 'light' | 'system';
+
+export type NoteSorting = 'title' | 'createdAt' | 'updatedAt';
+
+export type SessionErrorMessage = 'Session Expired' | 'Invalid Session';
+
+export type CollaboratorRole = 'editor' | 'viewer' | 'commenter';
+
 export type Context = Awaited<ReturnType<typeof context>>;
 
 export type Resolvers<S, A, C, I> = { [T: string]: (source: S, argument: A, context: C, info: I) => unknown; };
-
-export const enum ThemePreference {
-  DARK = 'dark',
-  LIGHT = 'light',
-  SYSTEM = 'system'
-}
 
 export interface Token {
   userId: string;
@@ -58,28 +60,33 @@ export interface ConnectProps {
 export interface UserExistsQueryFilter {
   id?: string;
   email?: string;
+  userId?: string;
   username?: string;
 }
 
 export interface GetUserQueryFilter {
   id?: string;
   email?: string;
+  userId?: string;
   username?: string;
+  includePassword?: boolean;
 }
 
 export interface DeleteUserQueryFilter {
   id?: string;
-  email?: string;
-  username?: string;
+  userId?: string;
+  loggedInUserId?: string;
 }
 
 export interface GetNoteQueryFilter {
   id?: string;
   userId: string;
+  noteId: string;
 }
 
-export interface GetNotesQueryFilter<T> extends Omit<GetNoteQueryFilter, 'id'>, Pick<PaginateQueryProps<T>, 'page' | 'limit'> {
+export interface GetNotesQueryFilter<T> extends Omit<GetNoteQueryFilter, 'id' | 'noteId'>, Pick<PaginateQueryProps<T>, 'page' | 'limit'> {
   authorId?: string;
+  query?: object;
 }
 
 export interface CreateNoteQueryFilter {
@@ -95,7 +102,7 @@ export interface DeleteNoteQueryFilter {
 export interface UpdateNoteQueryFilter {
   noteId: string;
   userId: string;
-  note: Partial<Note>;
+  noteDetails: Partial<Note>;
 }
 
 export interface SignupCredentials {
@@ -110,31 +117,159 @@ export interface LoginCredentials {
   password: string;
 }
 
-export interface Tag {
-  color: string;
-  label: string;
+export type NoteActivityAction =
+  | 'created note'
+  | 'updated note'
+  | 'added attachment(s)'
+  | 'removed attachment(s)'
+  | 'added collaborator(s)'
+  | 'removed collaborator(s)'
+  | 'transferred ownership';
+
+export interface GetNoteActivities {
+  noteId: string;
 }
 
-export interface User {
+export interface CreateNoteActivity {
+  action: NoteActivityAction;
+  initiator: string | User;
+  noteId: string;
+}
+
+export interface DeleteNoteActivitiesProps {
+  noteId: string;
+  activitiesId: string | string[];
+}
+
+export interface CreateCollaboratorProps {
+  userId: string;
+  noteId: string;
+  user: User;
+  role: CollaboratorRole;
+}
+
+export interface DeleteCollaboratorProps {
+  noteId: string;
+  userId: string;
+  collaboratorId: string;
+}
+
+export interface DeleteCollaboratorsProps {
+  userId: string;
+  noteId: string;
+  collaboratorsId: string | string[];
+}
+
+export interface CreateNoteProps {
+  note: Partial<Note>;
+  author: User;
+}
+
+export interface CanViewNoteProps {
+  note: Note;
+  userId?: string;
+}
+
+/* data types */
+
+export interface MongoDBObject {
   id: string;
-  name: string;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+
+export interface Tag extends MongoDBObject {
+  color: string;
+  label: string;
+  tagId: string;
+  userId: string;
+}
+
+export interface NoteActivity extends MongoDBObject {
+  action: string;
+  noteId: string;
+  initiator: User;
+  activityId: string;
+}
+
+export interface NoteAttachment extends MongoDBObject {
+  noteId: string;
+  fileType: string;
+  fileSize: number;
+}
+
+export interface Collaborator extends MongoDBObject {
+  /**
+   * foreign key of this collaborator in the notes collection
+   */
+  noteId: string;
+  /**
+   *  foreign key of this collaborator in the users collection
+   */
+  userId: string;
+  /**
+   *  primary key of this collaborator
+   */
+  collaboratorId: string;
+  /**
+   *  reference to the user who added(created) this collaborator
+   */
+  createdBy: User;
+  /**
+   *  collaborators role
+   */
+  role: CollaboratorRole;
+}
+
+export interface User extends MongoDBObject {
+  name: string | null;
   email: string;
+  userId: string;
   username: string;
-  password: string;
-  avatarUrl: string;
+  password?: string | null;
+  avatarUrl: string | null;
+  isDeactivated: boolean;
   isEmailVerified: boolean;
   preferences: {
-    theme: string;
+    theme: ThemePreference;
+    sortNoteBy: NoteSorting;
   };
 }
 
-export interface Note {
-  id: string;
+export interface Note extends MongoDBObject {
   tag: Tag;
+  author: User;
+  noteId: string;
+  authorId: string;
   title: string;
   content: string;
-  authorId: string;
+  isPinned: boolean;
   isPrivate: boolean;
+  isStarred: boolean;
   isArchived: boolean;
+  isAddedToTrash: boolean;
   visibility: NoteVisibility;
+  activities: NoteActivity[];
+  attachments: NoteAttachment[];
+  collaborators: Collaborator[];
+  starredBy: User[];
 }
+
+
+// 
+
+export type DeleteNote = (note: Note) => any;
+export type IsPrivateNote = (note: Note) => boolean;
+export type IsNoteAuthor = (user: User, note: Note) => boolean;
+export type IsNoteEditor = (user: User, note: Note) => boolean;
+export type IsNoteCollaborator = (user: User, note: Note) => boolean;
+export type CanViewNote = (note: Note, user: User | null) => boolean;
+
+
+export type DeleteAllNoteActivities = (note: Note) => any;
+export type CreateUpdateActivity = (note: Note, user: User) => any;
+
+export type DeleteNoteCollaborators = (note: Note, user: User) => any;
+
