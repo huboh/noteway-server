772997bfc93afa8ea52fd06f4c8c57967feb3c3e
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-17 00:32:54 +0100

    🤗 CHORE : add index file to `model` folder to re-export database models

---
+++ src/Database/models/index.ts
@@ -0,0 +1,5 @@
+export { default as Tag } from './Tag';
+export { default as User } from './User';
+export { default as Note } from './Note';
+export { default as NoteActivity } from './NoteActivity';
+export { default as Collaborator } from './Collaborator';
