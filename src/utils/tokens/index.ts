f20039db30dbe7d7b79d63895cd0c04f3cd0c64f
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-17 00:24:32 +0100

    🙈 CHORE : add error handlers for JWT errors

--- src/utils/tokens/index.ts
+++ src/utils/tokens/index.ts
@@ -1,21 +1,41 @@
-import jsonWebToken, { SignOptions, VerifyOptions } from 'jsonwebtoken';
+import errors from '../errors';
+import { isInstanceof } from '../';
+import jsonWebToken from 'jsonwebtoken';
+import { SESSION_EXPIRED_MESSAGE, SESSION_INVALID_MESSAGE } from '../constants';
 
 const { TOKEN_SECRET } = process.env;
 
-const generateToken = (payload: object | any[], options: SignOptions = {}) => (
+const generateToken = (payload: object, options: jsonWebToken.SignOptions = {}) => (
   jsonWebToken.sign(payload, TOKEN_SECRET!, {
     expiresIn: 28800, // 8hrs in seconds
     ...options
   })
 );
 
-const verifyToken = (token: string, options: VerifyOptions = {}) => (
+const verifyToken = (token: string, options: jsonWebToken.VerifyOptions = {}) => (
   jsonWebToken.verify(token, TOKEN_SECRET!, {
     ...options
   })
 );
 
+const handleTokenError = (error: unknown) => {
+  const { JsonWebTokenError, NotBeforeError, TokenExpiredError } = jsonWebToken;
+  const isJwtError = isInstanceof(error, [JsonWebTokenError, NotBeforeError, TokenExpiredError]);
+
+  if (!isJwtError) {
+    throw error;
+  }
+
+  const isExpiredToken = error.name === 'TokenExpiredError';
+  const errorMessage = isExpiredToken ? SESSION_EXPIRED_MESSAGE : SESSION_INVALID_MESSAGE;
+
+  throw new errors.AuthenticationError(
+    errorMessage
+  );
+};
+
 export default {
   verifyToken,
   generateToken,
-};;
+  handleTokenError
+};
