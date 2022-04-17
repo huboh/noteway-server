0057d5e5c5b5f76ed21d335454192d15065db242
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-17 11:23:02 +0100

    😌 CHORE : update note model schema

--- src/Database/models/Note.ts
+++ src/Database/models/Note.ts
@@ -1,13 +1,37 @@
-import { Note } from '../../types';
+import Tag from './Tag';
+import User from './User';
+import Collaborator from './Collaborator';
+import NoteActivity from './NoteActivity';
+import NoteAttachment from './NoteAttachment';
+
+import * as Types from '../../types';
+import * as Constants from '../../utils/constants';
+
 import { Schema, model } from 'mongoose';
-import { DEFAULT_NOTE_VISIBILITY } from '../../utils/constants';
+import mongooseAutoPopulate from 'mongoose-autopopulate';
+
 
 const NoteSchema = new Schema({
+  noteId: {
+    type: String, required: true
+  },
   authorId: {
-    type: Schema.Types.ObjectId, required: true,
+    type: String, required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    autopopulate: true,
+    required: true,
+    ref: User,
   },
   content: {
-    type: String, required: true
+    type: String, required: true, default: ''
+  },
+  isStarred: {
+    type: Boolean, default: false
+  },
+  isPinned: {
+    type: Boolean, default: false
   },
   isPrivate: {
     type: Boolean, default: false
@@ -15,27 +39,58 @@ const NoteSchema = new Schema({
   isArchived: {
     type: Boolean, default: false
   },
+  isAddedToTrash: {
+    type: Boolean, default: false
+  },
   visibility: {
     type: String,
     lowercase: true,
-    default: DEFAULT_NOTE_VISIBILITY
-  },
-  tag: {
-    color: { type: String, default: null, maxlength: 12 },
-    label: { type: String, default: null, maxlength: 120 }
+    default: Constants.DEFAULT_NOTE_VISIBILITY
   },
   title: {
     type: String,
-    lowercase: true,
-    required: [true, 'note title is required']
+    trim: true,
+    default: ''
   },
+  tag: {
+    type: Schema.Types.ObjectId,
+    ref: Tag,
+    default: null,
+    autopopulate: true,
+  },
+  activities: [{
+    type: Schema.Types.ObjectId,
+    ref: NoteActivity,
+    autopopulate: true,
+    default: [],
+  }],
+  starredBy: [{
+    type: Schema.Types.ObjectId,
+    ref: User,
+    autopopulate: true,
+    default: [],
+  }],
+  attachments: [{
+    type: Schema.Types.ObjectId,
+    ref: NoteAttachment,
+    autopopulate: true,
+    default: [],
+  }],
+  collaborators: [{
+    type: Schema.Types.ObjectId,
+    ref: Collaborator,
+    autopopulate: true,
+    default: [],
+  }],
 },
   {
     timestamps: true,
   }
 );
 
-const Note = model<Note>('Note', NoteSchema);
+NoteSchema.plugin(mongooseAutoPopulate);
+
+const Note = model<Types.Note>('Note', NoteSchema);
 
 export {
   Note as default
