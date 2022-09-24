40bd27d6cefe1a02030b40193932cbbcdb315ff5
Huboh <72440222+huboh@users.noreply.github.com>
2022-09-24 23:12:59 +0100

    REFACTOR: update import location for `constants`

--- src/utils/uuid/index.ts
+++ src/utils/uuid/index.ts
@@ -1,14 +1,16 @@
 import errors from '../errors';
 import { v4, validate } from 'uuid';
-import { INVALID_ID_ERROR_MESSAGE } from '../constants';
+import { INVALID_ID_ERROR_MESSAGE } from '../../constants';
 
 export const createUuid = () => v4();
 
-export const validateUuid = (...uuids: unknown[]) => uuids.forEach(uuid => {
-  if (uuid && !validate(uuid as string)) {
-    throw new errors.ValidationError(INVALID_ID_ERROR_MESSAGE);
-  }
-});
+export const validateUuid = (...uuids: unknown[]) => {
+  uuids.forEach((uuid) => {
+    if (uuid && !validate(uuid as string)) {
+      throw new errors.ValidationError(INVALID_ID_ERROR_MESSAGE);
+    }
+  });
+};
 
 export default {
   createUuid,
