c697abe7fcd52f7b92d016e3656df471f05eb4e1
Huboh <72440222+huboh@users.noreply.github.com>
2022-09-25 23:31:46 +0100

    CHORE: add new constants for graphql file paths

--- src/constants/graphql/index.ts
+++ src/constants/graphql/index.ts
@@ -1,1 +1,7 @@
+import { resolve } from "path";
+
 export const GraphQl_Path = '/api/graphql';
+
+export const GraphQl_Schema_Path = resolve(process.cwd(), "graphql/schema.graphql");
+export const GraphQl_Schema_Enums_Path = resolve(process.cwd(), "graphql/enums.graphql");
+export const GraphQl_Schema_Inputs_Path = resolve(process.cwd(), "graphql/inputs.graphql");
