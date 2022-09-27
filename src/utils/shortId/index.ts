b2be8fa7b79814cf47feb3e45501e11786ee2e25
Huboh <72440222+huboh@users.noreply.github.com>
2022-09-27 10:42:59 +0100

    REFACTOR: export functions individually

--- src/utils/shortId/index.ts
+++ src/utils/shortId/index.ts
@@ -1,7 +1,7 @@
 import { nanoid, customAlphabet } from "nanoid";
 
-const generateId = nanoid;
-const generateNumbers = (size?: number) => customAlphabet('0123456789', size ?? 6)();
+export const generateId = nanoid;
+export const generateNumbers = (size?: number) => customAlphabet('0123456789', size ?? 6)();
 
 export default {
   generateId,
